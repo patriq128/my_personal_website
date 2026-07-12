d81680bfc4448b344791fe298b945924bbdd67c2
patriq128 <121152391+patriq128@users.noreply.github.com>
2026-07-12 17:54:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch("https://api.github.com/users/patriq128/repos")
+.then(res => res.json())
+.then(repos => {
+
+    const container = document.getElementById("projects-container");
+
+    repos
+    .filter(repo => !repo.fork)
+    .sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at))
+    .forEach(repo => {
+
+        container.innerHTML += `
+        <a class="project-card" href="${repo.html_url}" target="_blank">
+
+            <h2>${repo.name}</h2>
+
+            <p>
+                ${repo.description || "No description"}
+            </p>
+
+            <p>
+                Last update: ${new Date(repo.pushed_at).toLocaleDateString()}
+            </p>
+
+        </a>
+        `;
+
+    });
+
+})
+.catch(error => {
+    console.error("GitHub API error:", error);
+});
